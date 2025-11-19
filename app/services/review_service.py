b7284097f54b9df82f9efde6be05e8d58e1cd745
Dias Maksatov <diasmaksatov@MacBook-Air-Dias.local>
2025-11-19 18:59:17 +0500

    Added review_service

--- app/services/review_service.py
+++ app/services/review_service.py
@@ -1,16 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from app.models.review_model import ReviewModel
+from sqlalchemy import select,update
+from app.models.review_model import ReviewModel,ReviewStatus
 from app.schemas.review_schema import ReviewCreate,ReviewResponse,ReviewUpdate
-
+from typing import List,Optional
 
 class ReviewService:
     def __init__(self,db_session:AsyncSession):
         self.db = db_session
     
 
-    async def create_review(self,review_data:ReviewCreate) -> ReviewModel:
-        review = ReviewModel(**review_data.model_dump())
+    async def create_review(self,review_data:ReviewCreate,user_id: int) -> ReviewModel:
+        review = ReviewModel(
+            **review_data.model_dump(),
+            user_id = user_id,
+            status = ReviewStatus.PENDING.value
+            )
         self.db.add(review)
         await self.db.commit()
         await self.db.refresh(review)
@@ -21,9 +25,17 @@ class ReviewService:
         reslut = await self.db.execute(query)
         return reslut.scalar_one_or_none
     
-    async def update_review(self,review_id:int,review_data:ReviewUpdate)->ReviewModel:
+    async def get_reviews_by_company(self, company_id: int, status: str | None = None, skip: int = 0, limit: int = 10) -> List[ReviewModel]:
+        query = select(ReviewModel).where(ReviewModel.company_id == company_id)
+        if status:
+            query = query.where(ReviewModel.status == status)
+        query = query.offset(skip).limit(limit)
+        result = await self.db.execute(query)
+        return result.scalars().all()
+    
+    async def update_review(self,review_id:int,review_data:ReviewUpdate,user_id:int)->ReviewModel:
         review = await self.get_review(review_id)
-        if not review:
+        if not review or review.user_id != user_id:
             return None
         for field,value in review_data.model_dump(exclude_unset=True).items():
             setattr(review,field,value)
@@ -32,11 +44,19 @@ class ReviewService:
         await self.db.refresh(review)
         return review
     
-    async def delete_review(self,review_id:int)->bool:
+    async def delete_review(self,review_id:int,user_id:int)->bool:
         review = await self.get_review(review_id)
-        if not review:
+        if not review or review.user_id != user_id:
             return False
         await self.db.delete(review)
         await self.db.commit()
         return True
         
+    async def moderate_review(self,review_id: int,status: ReviewStatus) ->Optional[ReviewModel]:
+        review = await self.get_review(review_id)
+        if not review:
+            return None
+        review.status = status.value
+        await self.db.commit()
+        await self.db.refresh(review)
+        return review
