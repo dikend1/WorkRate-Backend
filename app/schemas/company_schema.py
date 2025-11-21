a52291a5bda939e7b5211ca91bf06ebfb64be7d6
Dias Maksatov <diasmaksatov@MacBook-Air-Dias.local>
2025-11-22 01:41:40 +0500

    update company

---
+++ app/schemas/company_schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class CompanyBase(BaseModel):
+    name:str
+    description: str | None = None
+    website: str | None = None
+    industry: str | None = None
+    location: str | None = None
+    logo_url: str | None = None
+
+class CompanyCreate(CompanyBase):
+    pass
+
+class CompanyUpdate(BaseModel):
+    description: str | None = None
+    website: str | None = None
+    industry: str | None = None
+    location: str | None = None
+    logo_url: str | None = None
+
+class CompanyResponse(CompanyBase):
+    id:int
+    rating:float
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
