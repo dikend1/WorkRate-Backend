8a6f188ef1ae293bcc7fb59835aa55b0273841f8
Dias Maksatov <Maxatovdias@gmail.com>
2025-12-06 06:02:33 +0500

    added router for main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.db.base import Base
 from app.db.session import engine
 from app.api.routers.router_auth import router as auth_router
 from app.api.routers.router_companies import router as company_router
+from app.api.routers.router_salary import router as salary_router
+from app.api.routers.router_reviews import router as review_router
 import asyncio
 
 
@@ -10,6 +12,9 @@ app = FastAPI(title="IWork")
 
 app.include_router(auth_router)
 app.include_router(company_router)
+app.include_router(salary_router)
+app.include_router(review_router)
+
 
 @app.get("/")
 def root():
