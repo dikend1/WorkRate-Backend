be4dfe92099031f72caac843d0341503f3b27e31
Dias Maksatov <Maxatovdias@gmail.com>
2025-12-11 16:11:21 +0500

    added import all files

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,6 @@
 from .user_schema import UserBaseSchema, UserCreateSchema, UserResponseSchema, TokenSchema
+from .company_schema import CompanyBase, CompanyCreate, CompanyUpdate, CompanyResponse
+from .review_schema import ReviewBase, ReviewCreate, ReviewUpdate, ReviewResponse
+from .salary_schema import SalaryBase, SalaryCreate, SalaryUpdate, SalaryResponse
+from .account_settings_schema import AccountSettingsBase, AccountSettingsCreate, AccountSettingsUpdate, AccountSettingsResponse
+from .moderation_log_schema import ModerationLogBase, ModerationLogCreate, ModerationLogResponse
