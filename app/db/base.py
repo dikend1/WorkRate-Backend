102c2b3a9bde451b7c024265adf7f9b6a672fceb
Dias Maksatov <Maxatovdias@gmail.com>
2025-12-11 16:06:46 +0500

    import models

--- app/db/base.py
+++ app/db/base.py
@@ -7,4 +7,6 @@ from app.models.user_model import UserModel
 from app.models.company_model import CompanyModel
 from app.models.review_model import ReviewModel
 from app.models.salary_model import SalaryModel
+from app.models.account_settings_model import AccountSettings
+from app.models.moderation_log_model import ModerationLog
 
