029871ac65020c2b8b58aedddf83c2901b3465b1
Dias Maksatov <Maxatovdias@gmail.com>
2025-12-11 15:25:25 +0500

    added new column

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -15,6 +15,11 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String,nullable=True)
+    last_name = Column(String,nullable=True)
+    profile_picture= Column(String,nullable=True)
+    current_position = Column(String,nullable=True)
+    current_company_id = Column(Integer,nullable=True)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=True)  # Для OAuth пользователей пароль может быть null
